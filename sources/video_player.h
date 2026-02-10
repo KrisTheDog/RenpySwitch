937ca4a9e51ef5d43e87c20b52546598cbe4fb89
Gorag <greg6821@gmail.com>
2026-02-10 16:26:46 +0300

    Update video_player.h

--- sources/video_player.h
+++ sources/video_player.h
@@ -9,11 +9,11 @@ void play_video_file(const char *path, int skip_enabled);
 void play_video_file_delay(const char *path, int skip_enabled, float delay_seconds);
 void video_player_init();
 void video_player_quit();
-void show_logos(void);
 
 #ifdef __cplusplus
 }
 #endif
 
 #endif // VIDEO_PLAYER_H
+
 
