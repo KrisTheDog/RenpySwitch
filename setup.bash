454c6a8f9b55e7b361bc6907b18da625edd19bf5
Gorag <greg6821@gmail.com>
2026-06-06 00:18:52 +0300

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -4,25 +4,19 @@ export DEVKITPRO=/opt/devkitpro
 export RENPY_VER=7.6.3
 export PYGAME_SDL2_VER=2.1.0
 INITIAL_DIR=$(pwd)
+
 apt-get -y update
 apt-get -y upgrade
 
-apt -y install build-essential checkinstall
-apt -y install libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
-
-#apt -y install python2 python2-dev
-#python2 --version
-#curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
-#python2 get-pip.py
-#pip2 --version 
-
-
-
-
 # Устанавливаем зависимости для сборки Python и OpenSSL 1.1
-apt -y install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
+apt -y install build-essential checkinstall zlib1g-dev libncurses5-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libnss3-dev libreadline-dev libffi-dev wget
 
-# Скачиваем и собираем OpenSSL 1.1 (иначе pip2 не сможет скачивать пакеты из-за несовместимости с OpenSSL 3.x)
+# Устанавливаем зависимости для сборки хост-модулей Ren'Py
+apt-get -y install p7zip-full libsdl2-dev libsdl2-image-dev libjpeg-dev libpng-dev libsdl2-ttf-dev libsdl2-mixer-dev libavformat-dev libfreetype6-dev libswscale-dev libglew-dev libfribidi-dev libavcodec-dev libswresample-dev libsdl2-gfx-dev libgl1-mesa-glx
+
+# =========================================================
+# Скачиваем и собираем OpenSSL 1.1 (иначе pip2 не сможет скачивать пакеты)
+# =========================================================
 cd /tmp
 wget https://www.openssl.org/source/openssl-1.1.1w.tar.gz
 tar -xzvf openssl-1.1.1w.tar.gz
@@ -33,67 +27,53 @@ make install
 echo "/usr/local/openssl-1.1/lib" > /etc/ld.so.conf.d/openssl-1.1.conf
 ldconfig
 
-# Скачиваем и собираем Python 2.7.18 (последняя версия Python 2)
+# =========================================================
+# Скачиваем и собираем Python 2.7.18
+# =========================================================
 cd /tmp
 wget https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz
 tar -xzf Python-2.7.18.tgz
 cd Python-2.7.18
 
-# Указываем пути к собранному OpenSSL 1.1
 export CFLAGS="-I/usr/local/openssl-1.1/include"
 export LDFLAGS="-L/usr/local/openssl-1.1/lib"
 ./configure --enable-optimizations --prefix=/usr/local
 make -j$(nproc)
 make altinstall
 
-# Создаем симлинки, чтобы скрипт видел команды python2 и pip2
 ln -sf /usr/local/bin/python2.7 /usr/local/bin/python2
 ln -sf /usr/local/bin/pip2.7 /usr/local/bin/pip2
 
-# Очищаем временные файлы сборки
 rm -rf /tmp/openssl-1.1.1w /tmp/Python-2.7.18
 
+# Возвращаемся в рабочую папку!
 cd $INITIAL_DIR
 
-# Проверка
 python2 --version
 
-# Установка pip
+# Установка pip и зависимостей Python
 curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
 python2 get-pip.py
 pip2 --version 
 
-
-
-
-
-
-
-apt-get -y install p7zip-full libsdl2-dev libsdl2-image-dev libjpeg-dev libpng-dev libsdl2-ttf-dev libsdl2-mixer-dev libavformat-dev libfreetype6-dev libswscale-dev libglew-dev libfribidi-dev libavcodec-dev  libswresample-dev libsdl2-gfx-dev libgl1-mesa-glx
-pip2 uninstall distribute
+pip2 uninstall distribute -y || true
 pip2 install future six typing requests ecdsa pefile==2019.4.18 Cython==0.29.36 setuptools==0.9.8
 
-
-
-# Скачиваем только кастомный Python 2.7 для Switch (библиотеки теперь установим из репозитория)
+# =========================================================
+# Настройка окружения Switch
+# =========================================================
+# Скачиваем ТОЛЬКО кастомный Python 2.7 для Switch. 
+# Всё остальное (компилятор, SDL2, fribidi) УЖЕ есть в Docker-контейнере!
 curl -LOC - https://github.com/knautilus/Utils/releases/download/v1.0/python27-switch.zip
-
-# Устанавливаем ВСЁ необходимое для сборки под Switch (компилятор, cmake, sdl2, libfribidi и т.д.)
-# Это заменяет ручную установку devkitpro-pkgbuild-helpers и switch-libfribidi
-dkp-pacman -S --noconfirm switch-dev
-
-# Устанавливаем распакованный Python 2.7 для Switch
 unzip -qq python27-switch.zip -d $DEVKITPRO/portlibs/switch
 rm python27-switch.zip
 
-
-
-
+# =========================================================
+# Скачиваем исходники Ren'Py
+# =========================================================
 curl -LOC - https://www.renpy.org/dl/$RENPY_VER/pygame_sdl2-$PYGAME_SDL2_VER+renpy$RENPY_VER.tar.gz
 curl -LOC - https://www.renpy.org/dl/$RENPY_VER/renpy-$RENPY_VER-sdk.zip
 curl -LOC - https://www.renpy.org/dl/$RENPY_VER/renpy-$RENPY_VER-source.tar.bz2
-#curl -LOC - https://www.renpy.org/dl/$RENPY_VER/android-native-symbols.zip
-#curl -LOC - https://dl.otorh.in/github/rawproject.zip
 
 rm -rf pygame_sdl2-$PYGAME_SDL2_VER+renpy$RENPY_VER pygame_sdl2-source
 tar -xf pygame_sdl2-$PYGAME_SDL2_VER+renpy$RENPY_VER.tar.gz
@@ -108,25 +88,20 @@ rm renpy-$RENPY_VER-source.tar.bz2
 rm -rf renpy-$RENPY_VER-sdk renpy_sdk
 unzip -qq renpy-$RENPY_VER-sdk.zip -d renpy_sdk
 rm renpy-$RENPY_VER-sdk.zip
-cp -rf subprocess.pyo renpy_sdk/renpy-$RENPY_VER-sdk/lib/python2.7
 
-#dkp-pacman --noconfirm -S switch-libfribidi
+# Копируем кастомный subprocess.pyo (используем абсолютный путь от INITIAL_DIR)
+cp -rf $INITIAL_DIR/subprocess.pyo renpy_sdk/renpy-$RENPY_VER-sdk/lib/python2.7
 
-#rm -rf raw
-#unzip -qq rawproject.zip -d raw
-#rm rawproject.zip
-
-#rm -rf android-native-symbols renpy_androidlib ./raw/android/lib
-#unzip -qq android-native-symbols.zip -d ./raw/android/lib
-#rm -rf ./raw/android/lib/x86_64/
-#rm android-native-symbols.zip
-
+# =========================================================
+# Патчи и подготовка исходников
+# =========================================================
 pushd renpy-source
 patch -p1 < ../renpy.patch
 pushd module
 rm -rf gen gen-static
 popd
 popd
+
 pushd pygame_sdl2-source
 rm -rf gen gen-static
 popd
