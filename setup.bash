9da61eb9f129aebf373f2efaa6c7f172c07b94b0
Gorag <greg6821@gmail.com>
2026-06-05 21:35:34 +0300

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -73,17 +73,17 @@ apt-get -y install p7zip-full libsdl2-dev libsdl2-image-dev libjpeg-dev libpng-d
 pip2 uninstall distribute
 pip2 install future six typing requests ecdsa pefile==2019.4.18 Cython==0.29.36 setuptools==0.9.8
 
-curl -LOC - https://github.com/knautilus/Utils/releases/download/v1.0/devkitpro-pkgbuild-helpers-2.2.4-2-any.pkg.tar.xz
+#curl -LOC - https://github.com/knautilus/Utils/releases/download/v1.0/devkitpro-pkgbuild-helpers-2.2.4-2-any.pkg.tar.xz
 curl -LOC - https://github.com/knautilus/Utils/releases/download/v1.0/python27-switch.zip
-curl -LOC - https://github.com/knautilus/Utils/releases/download/v1.0/switch-libfribidi-1.0.12-1-any.pkg.tar.xz
+#curl -LOC - https://github.com/knautilus/Utils/releases/download/v1.0/switch-libfribidi-1.0.12-1-any.pkg.tar.xz
 
 # Удаляем строку установки старого пакета
 # dkp-pacman -U --noconfirm devkitpro-pkgbuild-helpers-2.2.4-2-any.pkg.tar.xz
-dkp-pacman -U --noconfirm switch-libfribidi-1.0.12-1-any.pkg.tar.xz
+#dkp-pacman -U --noconfirm switch-libfribidi-1.0.12-1-any.pkg.tar.xz
 unzip -qq python27-switch.zip -d $DEVKITPRO/portlibs/switch
 
-rm devkitpro-pkgbuild-helpers-2.2.4-2-any.pkg.tar.xz
-rm switch-libfribidi-1.0.12-1-any.pkg.tar.xz
+#rm devkitpro-pkgbuild-helpers-2.2.4-2-any.pkg.tar.xz
+#rm switch-libfribidi-1.0.12-1-any.pkg.tar.xz
 rm python27-switch.zip
 
 /bin/bash -c 'sed -i'"'"'.bak'"'"' '"'"'s/set(CMAKE_EXE_LINKER_FLAGS_INIT "/set(CMAKE_EXE_LINKER_FLAGS_INIT "-fPIC /'"'"' $DEVKITPRO/switch.cmake'
